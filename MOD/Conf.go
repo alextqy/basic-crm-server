a4b5c7a1636cf0681fecdf29a2f1d823bb1dc0c2
alextqy <289959263@qq.com>
2024-03-29 09:28:57 +0800

    coding

--- MOD/Conf.go
+++ MOD/Conf.go
@@ -1,6 +1,10 @@
 package mod
 
 type Conf struct {
+	DbHost  string `json:"db_host"`
+	DbPort  string `json:"db_port"`
+	DbUser  string `json:"db_user"`
+	DbPwd   string `json:"db_pwd"`
 	TcpPort string `json:"tcp_port"`
 	UdpPort string `json:"udp_port"`
 	Lang    string `json:"lang"`
